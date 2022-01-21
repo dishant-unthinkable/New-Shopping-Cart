3f2824135ae971fafe53da912eca2ac838b6108f
dishant-unthinkable <94823632+dishant-unthinkable@users.noreply.github.com>
2022-01-21 16:42:05 +0530

    Delete IOCIntro directory

--- IOCIntro/IOCIntro/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IOCIntro.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
